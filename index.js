c4257fb6e2b03c0f21985377099a4d7330622bb8
navneet9971 <Navneetjha2012@gmail.com>
2024-07-12 14:13:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config(); 
+const mongoose = require('mongoose');
+
+const login = require('./middleware/auth/login');
+const signup = require('./middleware/auth/signup');
+const profile = require('./middleware/auth/profile');
+
+const transactionPost = require('./middleware/transaction/transactionPost');
+const transactionGet = require('./middleware/transaction/transactionGet');
+ 
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // Exit process with failure
+  });
+
+// Test the server
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Route for authentication
+app.use('/api', login);
+app.use('/api', signup);
+app.use('/api', profile)
+
+//Route for transaction
+app.use('/api', transactionPost)
+app.use('/api', transactionGet)
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
---
+++ middleware/auth/signup.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const Auth = require("../../models/Auths");
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+router.post('/signup', async (req, res) => {
+    try {
+        const { firstName, lastName, email, password } = req.body;
+
+        if (!firstName || !lastName || !email || !password) {
+            return res.status(400).json({ error: 'All fields are required' });
+        }
+
+        const existingUser = await Auth.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ error: 'User already exists' });
+        }
+
+        // const hashedPassword = await bcrypt.hash(password, 10); // Hashing the password
+        const user = await Auth.create({ firstName, lastName, email, password });
+        res.status(201).json(user);
+    } catch (error) {
+        console.error('Error creating user:', error.message);
+        res.status(500).json({ error: 'Internal server error', message: error.message });
+    }
+});
+
+module.exports = router;
